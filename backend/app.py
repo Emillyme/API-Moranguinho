9b460c40883b7aeb1ac98756dcf6fbb7c4f88418
Emillyme <emill.ymello212@gmail.com>
2024-10-09 16:57:43 -0300

    API GET test

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-app = FastAPI()
+app: FastAPI = FastAPI()  # Anotação de tipo
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+    allow_origins=["http://localhost:3000"]
+)
 
 @app.get('/get_data')
 async def get_data():
-    return {'body': 'Rendereizar esse texto'}
+    return {'body': 'Emilly ama comer tomate que nem maçã'}
 
 if __name__ == '__main__':
-    uvicorn.run(app, port=8000)
+    uvicorn.run(app, port=9000)
